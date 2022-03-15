dd031568e6857473ec539eba807e3bd2062baefd
josecastilloUai <95188754+josecastilloUai@users.noreply.github.com>
2022-03-14 23:55:05 -0300

    procedimiento bienvenida
    Procedimiento que solicita el nombre del jugador. Luego se imprime un mensaje de bienvenida junto con las instrucciones, mencionando al nombre del jugador

--- main juego.c
+++ main juego.c
@@ -93,3 +93,12 @@ int main() {
   
   return 0;
 }
+
+void bienvenida(char nombre[]) {
+  // Procedimiento que solicita el nombre del jugador. Luego se imprime un mensaje de bienvenida junto con las instrucciones, mencionando al nombre del jugador.
+  printf("Saludos %s\n\n", nombre);
+  printf("---------- Bienvenido al juego del PESCAO ----------\n");
+  printf("EL PUENTE:\n\n");
+  printf("Aquí deberás elegir 0 (izquierda) o 1 (derecha), que será el lugar del puente hacia donde caminarás. Tú eres el 0. Pero cuidado, que si eliges mal MORIRÁS\n\n");
+  
+}
