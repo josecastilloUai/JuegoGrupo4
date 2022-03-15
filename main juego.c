a5dfcf640fba3ad3596681b9f0aa41bc1d2feec8
tdaiz9 <101015433+tdaiz9@users.noreply.github.com>
2022-03-14 23:51:04 -0300

    PROCEDIMIENTO TOMAS DIAZ MENU

--- main juego.c
+++ main juego.c
@@ -1,3 +1,19 @@
+void Menu(){
+  // Este procedimiento solicita ingresar un 0 o un 1 para empezar a jugar. Dependiendo de lo ingresado imprime mensaje distintos.
+  
+  int menu;
+  printf("\n\nPresione 1 para jugar / 0 para salir: ");
+	scanf("%d", &menu);
+  
+  if (menu == 1){
+    printf("Comienza el juego...\n");
+  }
+  else {
+    printf("\nVas a jugar igual.\n");
+    printf("Nadie se zafa de este juego.\n\n");
+  }
+}
+
 void puente(int intento, int lado)
 {
   // Procedimiento que solicita 2 números, el número de intento del juego, y el lado elegido por el jugador (0 o 1). Se genera el puente, el cual es una matriz de caracteres. Luego mediante un ciclo anidado se imprime la posición del jugador, dependiendo de los parametros dados.
